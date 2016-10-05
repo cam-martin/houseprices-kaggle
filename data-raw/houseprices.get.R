8a9b1785a95694d57dabe7efd0bce458c2d74518
Gabriele Baldassarre <gabriele.baldassarre@gmail.com>
2016-10-05 16:46:28 +0200

    better factorization for Boruta analysis

--- data-raw/houseprices.get.R
+++ data-raw/houseprices.get.R
@@ -12,27 +12,35 @@ houseprices.test <- read.csv(text = houseprices.data[2])
 
 ### houseprices.train adjustments
 houseprices.train$MSSubClass <- as.factor(houseprices.train$MSSubClass)
-houseprices.train$OverallQual <- factor(houseprices.train$OverallQual, levels=c("1","2","3","4","5","6","7","8","9","10"), ordered = TRUE)
-houseprices.train$OverallCond <- factor(houseprices.train$OverallCond, levels=c("1","2","3","4","5","6","7","8","9","10"), ordered = TRUE)
+houseprices.train$OverallQual <- factor(houseprices.train$OverallQual, levels=c("*MISSING*","1","2","3","4","5","6","7","8","9","10"), ordered = TRUE)
+houseprices.train$OverallCond <- factor(houseprices.train$OverallCond, levels=c("*MISSING*","1","2","3","4","5","6","7","8","9","10"), ordered = TRUE)
 
-houseprices.train$ExterQual <- factor(houseprices.train$ExterQual, levels=rev(c("Ex","Gd","TA","Fa","Po")), ordered=TRUE)
-houseprices.train$ExterCond <- factor(houseprices.train$ExterCond, levels=rev(c("Ex","Gd","TA","Fa","Po")), ordered=TRUE)
+houseprices.train$ExterQual <- factor(houseprices.train$ExterQual, levels=rev(c("Ex","Gd","TA","Fa","Po","*MISSING*")), ordered=TRUE)
+houseprices.train$ExterCond <- factor(houseprices.train$ExterCond, levels=rev(c("Ex","Gd","TA","Fa","Po","*MISSING*")), ordered=TRUE)
 
-houseprices.train$BsmtQual <- factor(houseprices.train$BsmtQual, levels=rev(c("Ex","Gd","TA","Fa","Po","NA")), ordered=TRUE)
-houseprices.train$BsmtCond <- factor(houseprices.train$BsmtCond, levels=rev(c("Ex","Gd","TA","Fa","Po","NA")), ordered=TRUE)
-houseprices.train$BsmtExposure <- factor(houseprices.train$BsmtExposure, levels=rev(c("Gd","Av","Mn","No","NA")), ordered=TRUE)
-houseprices.train$BsmtFinType1 <- factor(houseprices.train$BsmtFinType1, levels=rev(c("GLQ","ALQ","BLQ","Rec","LwQ","Unf","NA")), ordered=TRUE)
-houseprices.train$BsmtFinType2 <- factor(houseprices.train$BsmtFinType2, levels=rev(c("GLQ","ALQ","BLQ","Rec","LwQ","Unf","NA")), ordered=TRUE)
+houseprices.train$BsmtQual <- factor(houseprices.train$BsmtQual, levels=rev(c("Ex","Gd","TA","Fa","Po","NA","*MISSING*")), ordered=TRUE)
+houseprices.train$BsmtCond <- factor(houseprices.train$BsmtCond, levels=rev(c("Ex","Gd","TA","Fa","Po","NA","*MISSING*")), ordered=TRUE)
+houseprices.train$BsmtExposure <- factor(houseprices.train$BsmtExposure, levels=rev(c("Gd","Av","Mn","No","NA","*MISSING*")), ordered=TRUE)
+houseprices.train$BsmtFinType1 <- factor(houseprices.train$BsmtFinType1, levels=rev(c("GLQ","ALQ","BLQ","Rec","LwQ","Unf","NA","*MISSING*")), ordered=TRUE)
+houseprices.train$BsmtFinType2 <- factor(houseprices.train$BsmtFinType2, levels=rev(c("GLQ","ALQ","BLQ","Rec","LwQ","Unf","NA","*MISSING*")), ordered=TRUE)
 
-houseprices.train$HeatingQC <- factor(houseprices.train$HeatingQC, levels=rev(c("Ex","Gd","TA","Fa","Po")), ordered=TRUE)
-houseprices.train$KitchenQual <- factor(houseprices.train$KitchenQual, levels=rev(c("Ex","Gd","TA","Fa","Po")), ordered=TRUE)
-houseprices.train$FireplaceQu <- factor(houseprices.train$FireplaceQu, levels=rev(c("Ex","Gd","TA","Fa","Po","NA")), ordered=TRUE)
+houseprices.train$HeatingQC <- factor(houseprices.train$HeatingQC, levels=rev(c("Ex","Gd","TA","Fa","Po","*MISSING*")), ordered=TRUE)
+houseprices.train$KitchenQual <- factor(houseprices.train$KitchenQual, levels=rev(c("Ex","Gd","TA","Fa","Po","*MISSING*")), ordered=TRUE)
+houseprices.train$FireplaceQu <- factor(houseprices.train$FireplaceQu, levels=rev(c("Ex","Gd","TA","Fa","Po","NA","*MISSING*")), ordered=TRUE)
 
-houseprices.train$GarageQual <- factor(houseprices.train$GarageQual, levels=rev(c("Ex","Gd","TA","Fa","Po","NA")), ordered=TRUE)
-houseprices.train$GarageCond <- factor(houseprices.train$GarageQual, levels=rev(c("Ex","Gd","TA","Fa","Po","NA")), ordered=TRUE)
+houseprices.train$GarageQual <- factor(houseprices.train$GarageQual, levels=rev(c("Ex","Gd","TA","Fa","Po","NA","*MISSING*")), ordered=TRUE)
+houseprices.train$GarageCond <- factor(houseprices.train$GarageQual, levels=rev(c("Ex","Gd","TA","Fa","Po","NA","*MISSING*")), ordered=TRUE)
 
-houseprices.train$PoolQC <- factor(houseprices.train$PoolQC, levels=rev(c("Ex","Gd","TA","Fa","NA")), ordered=TRUE)
-houseprices.train$Fence <- factor(houseprices.train$Fence, levels=rev(c("GdPrv","MnPrv","GdWo","MnWw","NA")), ordered=TRUE)
+houseprices.train$PoolQC <- factor(houseprices.train$PoolQC, levels=rev(c("Ex","Gd","TA","Fa","NA","*MISSING*")), ordered=TRUE)
+houseprices.train$Fence <- factor(houseprices.train$Fence, levels=rev(c("GdPrv","MnPrv","GdWo","MnWw","NA","*MISSING*")), ordered=TRUE)
+
+houseprices.train$Alley <- factor(houseprices.train$Alley, levels=c("Grvl", "Pane", "*MISSING*"))
+houseprices.train$MasVnrType <- factor(houseprices.train$MasVnrType, levels=c("BrkCmn", "BrkFace", "None", "Stone", "*MISSING*"))
+houseprices.train$Electrical <- factor(houseprices.train$Electrical, levels=c("FuseA", "FuseF", "FuseP", "Mix","SBrkr", "*MISSING*"))
+houseprices.train$GarageType <- factor(houseprices.train$GarageType, levels=c("2Types", "Attchd", "Basment", "BuiltIn","CarPort", "Detchd", "*MISSING*"))
+houseprices.train$GarageFinish <- factor(houseprices.train$GarageFinish, levels=c("RFn", "Unf", "Fin", "*MISSING*"))
+houseprices.train$MiscFeature <- factor(houseprices.train$MiscFeature, levels=c("Gar2", "Othr", "Shed", "*MISSING*"))
+
 
 #YearBuilt
 #YearRemodAdd
